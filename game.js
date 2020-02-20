ac93f2d50cde99049fe6354b8832cdae385d2753
3ineck <60150729+3ineck@users.noreply.github.com>
2020-02-20 11:13:58 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,118 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+//Start the game
+
+$(document).on("keypress", function () {
+
+  if (!started){
+
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+
+}
+
+});
+
+// User chosen button
+
+$(".btn").click(function () {
+
+var userChosenColour = $(this).attr("id");
+userClickedPattern.push(userChosenColour);
+
+playSound(userChosenColour);
+animatePress($(this).attr("id"));
+
+checkAnswer(userClickedPattern.length-1);
+
+});
+
+//Play sounds
+
+function playSound(name) {
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+
+};
+
+//Animations
+
+function animatePress(currentColour) {
+
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout (function () {
+  $("#" + currentColour).removeClass("pressed");
+}, 100);
+
+};
+
+// Generate random button
+
+function nextSequence() {
+
+  userClickedPattern = [];
+
+  var randomNumber = Math.floor(Math.random()*4);
+
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+
+  level++;
+
+  $("#level-title").text("Level " + level);
+
+};
+
+//Check Awnser
+
+function checkAnswer(currentLevel) {
+
+if (userClickedPattern[currentLevel] === gamePattern [currentLevel]) {
+
+  if (userClickedPattern.length === gamePattern.length) {
+
+    setTimeout (function () {
+    nextSequence();
+  }, 1000);
+
+}
+}
+
+else {
+
+  playSound("wrong");
+  $("body").addClass("game-over");
+
+  setTimeout (function () {
+  $("body").removeClass("game-over");
+}, 200);
+
+  $("#level-title").text("Game Over, Press Any Key to Restart!");
+
+  startOver();
+
+}
+
+};
+
+// Restart the game
+
+function startOver() {
+
+  level = 0;
+  gamePattern = [];
+  started = false;
+
+}
